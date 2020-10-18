5e446f4ef830918a23b1ea9bf464aed0663c3cc0
Furkan Cayci <furkanca02@gmail.com>
2020-10-18 08:40:00 +0300

    added blinky example

---
+++ blinky/main.c
@@ -0,0 +1,42 @@
+/*
+ * main.c
+ *
+ * author: Furkan Cayci
+ *
+ * description: Blinks 1 on-board LED at roughly 1 second intervals. system 
+ *   clock is running from HSI which is 16 Mhz. Delay function is just a simple 
+ *   counter so is not accurate and the delay time will change based on the 
+ *   optimization flags.
+ */
+
+#include "stm32g0xx.h"
+
+#define LEDDELAY    1600000
+
+int main(void);
+void delay(volatile uint32_t);
+
+int main(void) {
+
+    /* Enable GPIOC clock */
+    RCC->IOPENR |= (1U << 2);
+
+    /* Setup PC6 as output */
+    GPIOC->MODER &= ~(3U << 2*6);
+    GPIOC->MODER |= (1U << 2*6);
+
+    /* Turn on LED */
+    GPIOC->ODR |= (1U << 6);
+
+    while(1) {
+        delay(LEDDELAY);
+        /* Toggle LED */
+        GPIOC->ODR ^= (1U << 6);
+    }
+
+    return 0;
+}
+
+void delay(volatile uint32_t s) {
+    for(; s>0; s--);
+}
---
+++ blinky/makefile
@@ -0,0 +1,12 @@
+TARGET = blinky
+SRCS = main.c
+
+LINKER_SCRIPT = ../linker/STM32G031K8Tx_FLASH.ld
+
+# Generate debug info
+DEBUG = 1
+
+# Choose processor
+CDEFS  = -DSTM32G031xx
+
+include ../armg0.mk
