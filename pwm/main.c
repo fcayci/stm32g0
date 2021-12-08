7be8ad0a136aebb5b3b2fa590c1528cd112639e0
Furkan Cayci <furkanca02@gmail.com>
2021-12-08 16:33:52 +0300

    pwm added pin configs

--- pwm/main.c
+++ pwm/main.c
@@ -11,15 +11,34 @@ void TIM2_IRQHandler(void) {
     TIM2->SR &= ~(1U << 0); 
 }
 
+
+/**
+ * Setup PWM output for
+ * TIM2 CH2 on PB3
+ */
 void init_pwm2() {
 
-    // enable TIM2 module clock
+    // Setup GPIO
+    //
+
+    // Enable GPIOB clock
+    RCC->IOPENR |= (1U << 1);
+    // Enable TIM2 clock
     RCC->APBENR1 |= RCC_APBENR1_TIM2EN;
+
+    // Set alternate function to 2
+    // 3 comes from PB3
+    GPIOB->AFR[0] |= (2U << 4*3);
+    // Select AF from Moder
+    GPIOB->MODER &= ~(3U << 2*3);
+    GPIOB->MODER |= (2U << 2*3);
 
     // zero out the control register just in case
     TIM2->CR1 = 0;
 
+    // Select PWM Mode 1
     TIM2->CCMR1 |= (6U << 12);
+    // Preload Enable
     TIM2->CCMR1 |= TIM_CCMR1_OC2PE;
 
     // Capture compare ch2 enable
@@ -38,7 +57,7 @@ void init_pwm2() {
     TIM2->DIER |= (1 << 0);
 
     // TIM1 Enable
-    TIM2->CR1 |= (1 << 0);
+    TIM2->CR1 |= TIM_CR1_CEN;
 
     NVIC_SetPriority(TIM2_IRQn, 1);
     NVIC_EnableIRQ(TIM2_IRQn);
